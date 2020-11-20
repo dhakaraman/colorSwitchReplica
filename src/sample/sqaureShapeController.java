92c5838d462a627a005fc6edd8d7eb01c772cb67
ankitgarg2001 <ankitgarg2001>
2020-11-20 12:40:57 +0530

    commit16

--- src/sample/sqaureShapeController.java
+++ src/sample/sqaureShapeController.java
@@ -1,6 +1,7 @@
 package sample;
 import javafx.animation.*;
 import javafx.fxml.FXML;
+import javafx.geometry.Point3D;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.Line;
 import javafx.scene.transform.Rotate;
@@ -26,18 +27,41 @@ public class sqaureShapeController {
     @FXML
     private void initialize() {
         Rotate rotation = new Rotate();
-        rotation.pivotXProperty().bind(line1.layoutXProperty());
-        rotation.pivotYProperty().bind(line1.layoutYProperty());
+        Rotate r2 = new Rotate();
+        Rotate r3 = new Rotate();
+        Rotate r4 = new Rotate();
+        rotation.setPivotX(50);
+        rotation.setPivotY(150);
         line1.getTransforms().add(rotation);
-        line2.getTransforms().add(rotation);
-        line3.getTransforms().add(rotation);
-        line4.getTransforms().add(rotation);
+
+        r2.setPivotX(50);
+        r2.setPivotY(-50);
+        line2.getTransforms().add(r2);
+        r3.setPivotX(50);
+        r3.setPivotY(50);
+        line3.getTransforms().add(r3);
+        r4.setPivotX(50);
+        r4.setPivotY(50);
+        line4.getTransforms().add(r4);
+
+
+        timeset(rotation, r2);
+
+        timeset(r3, r4);
+
+
+    }
+
+    private void timeset(Rotate rotation, Rotate r2) {
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.ZERO, new KeyValue(rotation.angleProperty(), 0)),
                 new KeyFrame(Duration.seconds(1000), new KeyValue(rotation.angleProperty(), 36000)));
         timeline.play();
 
-
+        Timeline t2 = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(r2.angleProperty(), 0)),
+                new KeyFrame(Duration.seconds(1000), new KeyValue(r2.angleProperty(), 36000)));
+        t2.play();
     }
 
 }
